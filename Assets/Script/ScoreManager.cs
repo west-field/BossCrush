66e7a848424756be658edcef675ff74da2feece8
west-field <fko2247034@stu.o-hara.ac.jp>
2024-10-18 16:18:07 +0900

    csvファイルに得点を保存させる

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -8,12 +8,61 @@ public class ScoreManager : MonoBehaviour
     private int score;
     [SerializeField] private TextMeshProUGUI scoreText;
 
+    private WriteReadToCSV scoreData;
+
     private void Start()
     {
         score = 0;
         scoreText.text = this.score.ToString();
+
+        if(GetComponent<WriteReadToCSV>() != null)
+        {
+            scoreData = GetComponent<WriteReadToCSV>();
+        }
+
+        if (scoreData != null)
+        {
+            Debug.Log("ì«Ç›çûÇ›");
+            scoreData.ReadDataToCSV();
+            Debug.Log(scoreData.Data());
+        }
     }
 
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        if(scoreData != null)
+        {
+            var index = 0;
+
+            Debug.Log("ï€ë∂");
+            scoreData.InitializeData(1);
+            scoreData.SetDataAt(index, score);
+            scoreData.AddData();
+
+            var list = scoreData.Data();
+            Debug.Log(list.Count.ToString());
+            if (list.Count >= 2)
+            {
+                var s = list[1];
+                scoreData.SetDataAt(index, Mathf.Max(score, int.Parse(s[0])));
+            }
+            else
+            {
+                scoreData.SetDataAt(index, score);
+            }
+            scoreData.AddData();
+            scoreData.WriteDataToCSV();
+        }
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    /// <summary> ÉXÉRÉAÇí«â¡Ç∑ÇÈ </summary>
+    /// <param name="score"></param>
     public void AddScore(int score)
     {
         this.score += score;
